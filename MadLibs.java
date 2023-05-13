b1cc66c43131d1eae5e488b49d4e2bc32ed76acf
happiness <20544603+happinessk@users.noreply.github.com>
2023-05-12 23:11:25 -0500

    Update MadLibs.java

--- MadLibs.java
+++ MadLibs.java
@@ -3,6 +3,9 @@ public class MadLibs {
   This program generates a mad libbed story.
 Author: Happiness
 Date: 5/12/2023
+
+In this project, we’ll use Java to write a Mad Libs word game! Mad Libs have short stories with blank spaces that a player can fill in. The result is usually funny (or strange).
+Mad Libs require: A short story with blank spaces (asking for different types of words). Words from the player to fill in those blanks.
   */
   	public static void main(String[] args){ 
       String name1 = "Teddy";
